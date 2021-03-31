31ff69ec4beb0089102cb52061febb7cff454452
YuBin Moon <moon.pinnamon@gmail.com>
2021-03-31 16:20:31 +0900

    reorder Log File (using Lambda)

--- MultiTest.py
+++ MultiTest.py
@@ -1,5 +1,6 @@
 import unittest
 from validPalindrome import Solution as ValidPalindrome
+from reorderDataInLogFiles import Solution as ReorderLogFiles
 
 
 class MyTestCase(unittest.TestCase):
@@ -18,6 +19,20 @@ class ValidPalindromeTestCae(unittest.TestCase):
     def test_second_case(self):
         ans = self.sol.isPalindrome(s="race a car")
         self.assertEqual(ans, False)
+
+
+class ReorderLogFilesTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sol = ReorderLogFiles()
+
+    def test_first_case(self):
+        ans = self.sol.reorderLogFiles(
+            logs=["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"])
+        self.assertEqual(ans, ["let1 art can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1", "dig2 3 6"])
+
+    def test_second_case(self):
+        ans = self.sol.reorderLogFiles(logs=["a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"])
+        self.assertEqual(ans, ["g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"])
 
 
 if __name__ == '__main__':
