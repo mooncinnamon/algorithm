4fc9d65e63a62a6585a11cb4aefc9b0f30679081
YuBin Moon <moon.pinnamon@gmail.com>
2021-03-31 17:20:28 +0900

    most Common Word

--- MultiTest.py
+++ MultiTest.py
@@ -1,6 +1,7 @@
 import unittest
 from validPalindrome import Solution as ValidPalindrome
 from reorderDataInLogFiles import Solution as ReorderLogFiles
+from mostCommonWord import Solution as MostCommonWord
 
 
 class MyTestCase(unittest.TestCase):
@@ -33,6 +34,20 @@ class ReorderLogFilesTestCase(unittest.TestCase):
     def test_second_case(self):
         ans = self.sol.reorderLogFiles(logs=["a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"])
         self.assertEqual(ans, ["g1 act car", "a8 act zoo", "ab1 off key dog", "a1 9 2 3 1", "zo4 4 7"])
+
+
+class MostCommonWordTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sol = MostCommonWord()
+
+    def test_first_case(self):
+        ans = self.sol.mostCommonWord(paragraph="Bob hit a ball, the hit BALL flew far after it was hit.",
+                                      banned=["hit"])
+        self.assertEqual(ans, "ball")
+
+    def test_second_case(self):
+        ans = self.sol.mostCommonWord(paragraph="a.", banned=[])
+        self.assertEqual(ans, "a")
 
 
 if __name__ == '__main__':
